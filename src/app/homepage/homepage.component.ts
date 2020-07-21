8bca5a457e2b81b332bded25f6a27739a8ebc117
gesab001 <144000giovanni@gmail.com>
2020-07-21 14:16:34 +1200

    added activities

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Homepage } from './homepage';
+import { HomepageService} from './homepage.service';
+import { filter, map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [HomepageService]
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  lessons: any;
+  subscription;
+  constructor(private route: ActivatedRoute, private homepageService: HomepageService) { }
 
   ngOnInit(): void {
+     this.loadData();
   }
 
+ loadData() {
+    this.subscription = this.homepageService.getData().subscribe(
+      res => (this.lessons = res),
+      error => console.log(error),
+    );
+  }
 }
--- src/app/homepage/homepage.service.ts
+++ src/app/homepage/homepage.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { StateGroup } from './stategroup';
 import { catchError } from 'rxjs/operators';
-import { HttpErrorHandler, HandleError } from './http-error-handler.service';
+import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
 import { tap } from 'rxjs/operators';
 import {publishReplay, refCount} from 'rxjs/operators';
 
@@ -14,7 +13,7 @@ export class HomepageService {
   _data: any = null;
 
   private handleError: HandleError;
-  url = 'https://gesab001.github.io/assets/homeschool/activities.json';
+  url = 'https://gesab001.github.io/assets/homeschool/math/math_year_1.json';
   constructor(
     private http: HttpClient,
     httpErrorHandler: HttpErrorHandler) {
